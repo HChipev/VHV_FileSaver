3fa90e84be95f448aec5f099863414433324c12a
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-07-24 12:33:37 +0300

    Add Extend Session and Middleware (#3)

--- src/FrontEnd/nuxt.config.ts
+++ src/FrontEnd/nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  runtimeConfig: {
+    private: {
+      jwt_key: process.env.JWT_KEY,
+    },
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
